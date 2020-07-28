1973bab3b02a3927829e4bf84a53aab945970bbd
Jacob Miller <millerjacob@seattleu.edu>
2020-07-28 13:50:32 -0700

    Added Keg Schema

---
+++ type-server/src/models/keg.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Beer = mongoose.model('Beer');
+const Customer = mongoose.model('Customer');
+
+const kegSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    size: {
+        type: String,
+        required: true
+    },
+    beer: {
+        Beer,
+        required: true
+    },
+    fillDate: {
+        Date,
+        required:true
+    },
+    clean:{
+        type:Boolean,
+        required:true
+    },
+    cleanDate: {
+        Date,
+        required:true
+    },
+    customer: {
+        Customer,
+        required: true
+    },
+    pickupDate: Date,
+    returnDate: Date,
+    returnType: String,
+});
+
+mongoose.model('keg', kegSchema);
