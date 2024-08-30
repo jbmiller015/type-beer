8683405d0618e97ee42178d93940a87ea85534df
Jacob Miller <jbmiller015@gmail.com>
2024-08-29 21:27:23 -0400

    Provision for edit and deleting default data

--- type-app/src/api/type-server.js
+++ type-app/src/api/type-server.js
@@ -12,22 +12,30 @@ const instance = axios.create({
 const appendSessionResponse = (responseData, url) => {
     const sessionObj = JSON.parse(sessionStorage.getItem(url));
     const editObj = JSON.parse(sessionStorage.getItem(`edit${url}`));
+    const deleteObj = JSON.parse(sessionStorage.getItem(`delete${url}`));
     console.log(sessionObj)
+    let result;
     if (editObj) {
-        responseData = responseData.map((el) => {
+        result = responseData.map((el) => {
+
             for (let le in editObj) {
+
                 if (editObj[le]._id === el._id) {
+
                     return editObj[le];
                 }
             }
             return el;
         });
     }
-    if (sessionObj) {
-        responseData.push(...sessionObj)
-        return responseData;
+    console.log(result)
+    if (deleteObj) {
+        result = result.filter((el) => !deleteObj.includes(el._id));
     }
-
+    if (sessionObj) {
+        result.push(...sessionObj)
+    }
+    return result;
 }
 
 instance.interceptors.response.use((response) => {
@@ -113,11 +121,11 @@ instance.interceptors.request.use(
                 //TODO: 'replace' default objs with editted ones by setting session data and replacing json data in get requests.
                 //[blank,demo,objName,Id]
                 const modder = config.url.split('/');
-                console.log(modder)
+                //console.log(modder)
                 const sessionObj = JSON.parse(sessionStorage.getItem(`/${modder[1]}/${modder[2]}`));
-                console.log(sessionObj)
+                //console.log(sessionObj)
                 const adder = config.data;
-                console.log(adder)
+                // console.log(adder)
                 let result;
                 if (sessionObj) {
                     result = sessionObj.map((el) => {
@@ -125,7 +133,7 @@ instance.interceptors.request.use(
                             return adder;
                         return el;
                     });
-                    console.log(result)
+                    // console.log(result)
                     sessionStorage.setItem(`/${modder[1]}/${modder[2]}`, JSON.stringify(result))
                 } else {
                     const editObj = JSON.parse(sessionStorage.getItem(`edit/${modder[1]}/${modder[2]}`));
@@ -135,7 +143,7 @@ instance.interceptors.request.use(
                                 return adder;
                             return el;
                         });
-                        console.log(result)
+                        // console.log(result)
                         sessionStorage.setItem(`edit/${modder[1]}/${modder[2]}`, JSON.stringify(result))
                     } else {
                         sessionStorage.setItem(`edit/${modder[1]}/${modder[2]}`, JSON.stringify([adder]));
@@ -158,28 +166,43 @@ instance.interceptors.request.use(
 
             }
             if (config.method === 'delete') {
-                //[blank,demo,objName,Id]
-                const modder = config.url.split('/');
-                const sessionObj = JSON.parse(sessionStorage.getItem(`/${modder[1]}/${modder[2]}`));
-                const adder = config.data;
-                let result;
-                if (sessionObj) {
-                    result = sessionObj.filter((el) => el._id !== modder[3]);
-                    sessionStorage.setItem(`/${modder[1]}/${modder[2]}`, JSON.stringify(result))
-                }
+                try {
+                    //[blank,demo,objName,Id]
+                    const modder = config.url.split('/');
+                    console.log("modder:", modder)
+                    const sessionObj = JSON.parse(sessionStorage.getItem(`${modder[1]}/${modder[2]}`));
+                    let deleteObj = JSON.parse(sessionStorage.getItem(`delete/${modder[1]}/${modder[2]}`));
+                    const adder = "config.data";
+                    let result;
+                    if (sessionObj) {
+                        result = sessionObj.filter((el) => el._id !== modder[3]);
+                        console.log("result:", result)
+                        sessionStorage.setItem(`${modder[1]}/${modder[2]}`, JSON.stringify(result));
+                    } else if (deleteObj) {
+                        deleteObj = deleteObj.push(modder[3]);
+                        console.log("deleteObj:", deleteObj)
+                        sessionStorage.setItem(`delete/${modder[1]}/${modder[2]}`, JSON.stringify(deleteObj))
 
-                config.adapter = (config) => {
-                    return new Promise((resolve, reject) => {
-                        const res = {
-                            data: "",
-                            status: 200,
-                            statusText: "OK",
-                            headers: {"content-type": "text/plain; charset=utf-8"},
-                            config,
-                            request: {}
-                        }
-                        return resolve(res);
-                    })
+                    } else {
+                        console.log("deleteObj.push(modder[3]):", [modder[3]])
+                        sessionStorage.setItem(`delete/${modder[1]}/${modder[2]}`, JSON.stringify([modder[3]]))
+                    }
+
+                    config.adapter = (config) => {
+                        return new Promise((resolve, reject) => {
+                            const res = {
+                                data: "",
+                                status: 200,
+                                statusText: "OK",
+                                headers: {"content-type": "text/plain; charset=utf-8"},
+                                config,
+                                request: {}
+                            }
+                            return resolve(res);
+                        })
+                    }
+                } catch (err) {
+                    console.log(err)
                 }
             }
         }
